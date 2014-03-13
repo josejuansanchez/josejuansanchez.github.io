5efde798646d9c44823424efa83d9e2e02e10775
José Juan Sánchez <josejuan.sanchez@gmail.com>
2014-03-13 14:15:16 +0100

    Create test_network.sh

---
+++ notebook/test_network.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=responses.txt
+touch $FILE
+
+SUBNET=192.168.119
+
+for ip in $(seq 1 254); 
+do 
+	echo "$SUBNET.$ip..."
+	ping -c $SUBNET.$ip >> $FILE 2>&1
+done
+
+TOTAL_IPS=`cat $FILE | grep rtt | wc -l`
+echo -e "\n-----------------"
+echo "#Total IPs: $TOTAL_IPS"
+echo -e "-----------------"
