6bdcc115d35be94aa4cd5a5977fee23bffc543f6
José Juan Sánchez <josejuan.sanchez@gmail.com>
2014-03-13 14:19:18 +0100

    Update test_network.sh

--- notebook/test_network.sh
+++ notebook/test_network.sh
@@ -3,12 +3,12 @@
 FILE=responses.txt
 touch $FILE
 
-SUBNET=192.168.119
+SUBNET="192.168.119"
 
 for ip in $(seq 1 254); 
 do 
 	echo "$SUBNET.$ip..."
-	ping -c $SUBNET.$ip >> $FILE 2>&1
+	ping -c 1 $SUBNET.$ip >> $FILE 2>&1
 done
 
 TOTAL_IPS=`cat $FILE | grep rtt | wc -l`
